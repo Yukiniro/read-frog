b02ecf5705cee110ef9d2e2044f9a9a262f7c08a
Zhanghao <yukiniro@hotmail.com>
2025-08-26 13:41:58 +0800

    feat(extension): support to clear cache

--- apps/extension/src/entrypoints/options/pages/general/index.tsx
+++ apps/extension/src/entrypoints/options/pages/general/index.tsx
@@ -1,5 +1,6 @@
 import { i18n } from '#imports'
 import { PageLayout } from '../../components/page-layout'
+import { ClearCacheConfig } from './clear-cache-config'
 import { ReadConfig } from './read-config'
 import TranslationConfig from './translation-config'
 
@@ -8,6 +9,7 @@ export function GeneralPage() {
     <PageLayout title={i18n.t('options.general.title')} innerClassName="[&>*]:border-b [&>*:last-child]:border-b-0">
       <TranslationConfig />
       <ReadConfig />
+      <ClearCacheConfig />
     </PageLayout>
   )
 }
