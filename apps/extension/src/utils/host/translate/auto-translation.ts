68a66127784aada4d41f0d38d0e1c3d4e81a2397
Yukiniro <yukiniro@hotmail.com>
2025-09-06 10:58:38 +0800

    fix: automatic translation language matching supports user-selected language

--- apps/extension/src/utils/host/translate/auto-translation.ts
+++ apps/extension/src/utils/host/translate/auto-translation.ts
@@ -3,12 +3,13 @@ import type { Config } from '@/types/config/config'
 export async function shouldEnableAutoTranslation(url: string, config: Config): Promise<boolean> {
   const autoTranslatePatterns = config?.translate.page.autoTranslatePatterns
   const autoTranslateLanguages = config?.translate.page.autoTranslateLanguages
+  const { detectedCode, sourceCode } = config?.language || {}
 
   const doesMatchPattern = autoTranslatePatterns?.some(pattern =>
     url.toLowerCase().includes(pattern.toLowerCase()),
   ) ?? false
 
-  const doesMatchLanguage = autoTranslateLanguages?.includes(config?.language.detectedCode)
+  const doesMatchLanguage = autoTranslateLanguages?.includes(sourceCode === 'auto' ? detectedCode : sourceCode)
 
   return doesMatchPattern || doesMatchLanguage
 }
